65ecaea2e751ce8d60420c36cf72182b4f969648
Lotw <qkrtmd5@naver.com>
2023-10-09 20:55:21 +0900

    3주차 풀이

---
+++ seungmin/Week3/1002/BOJ_2754.py
@@ -0,0 +1,28 @@
+p = input()
+
+if p == "A+":
+    print("4.3")
+elif p == "A0":
+    print("4.0")
+elif p == "A-":
+    print("3.7")
+elif p == "B+":
+    print("3.3")
+elif p == "B0":
+    print("3.0")
+elif p == "B-":
+    print("2.7")
+elif p == "C+":
+    print("2.3")
+elif p == "C0":
+    print("2.0")
+elif p == "C-":
+    print("1.7")
+elif p == "D+":
+    print("1.3")
+elif p == "D0":
+    print("1.0")
+elif p == "D-":
+    print("0.7")
+else:
+    print("0.0")
